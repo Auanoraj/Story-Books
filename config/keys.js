a138d54852547d0e814b1cc6fb36b0383697afc9
Rakshith Raj <auanoraj@live.com>
2018-06-17 22:42:45 +0530

    Config file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://auanoraj:deepak23@ds163610.mlab.com:63610/storybookz',
-    secretKey: 'secret'
+    mongoURI: "Enter your MongoDB URI",
+    secretKey: ""
 }
